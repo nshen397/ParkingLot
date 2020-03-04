8358e149bf65335276b86b84ba1868a555b26bab
nshen397 <38293733+nshen397@users.noreply.github.com>
2020-03-04 10:31:20 -0700

    Initial

---
+++ ParkingApp/app/src/main/java/com/example/parkingapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void spot(View view) {
+        String tag = view.getTag().toString();
+    }
+}
